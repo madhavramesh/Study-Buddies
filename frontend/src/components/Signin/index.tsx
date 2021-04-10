fcf39555cbe991afe33a31adae2bde7ea8c16e94
Madhav Ramesh <madhavramesh02@gmail.com>
2021-04-10 08:08:54 -0400

    Added link from signin to signup page

--- frontend/src/components/Signin/index.tsx
+++ frontend/src/components/Signin/index.tsx
@@ -43,44 +43,52 @@ const Signin: React.FC = () => {
   };
 
   return (
-    <Form className="signin">
-      <Form.Group controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          required
-          isInvalid={!validAcct}
-          onChange={(e: any) => setEmail(e.target.value)}
-        />
-        <Form.Control.Feedback type="invalid">{validAcctMessage}</Form.Control.Feedback>
-      </Form.Group>
+    <div className="signin-container">
+      <div className="signin-header">
+        <div className="signin-header-text">Sign in</div>
+        <div className="signup">
+          or&nbsp;<Link to="/signup">create an account</Link>
+        </div>
+      </div>
+      <Form className="signin">
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            required
+            isInvalid={!validAcct}
+            onChange={(e: any) => setEmail(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">{validAcctMessage}</Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Group controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          required
-          isInvalid={!validAcct}
-          onChange={(e: any) => setPassword(e.target.value)}
-        />
-        <Form.Control.Feedback type="invalid" />
-      </Form.Group>
-      <p className="forgot-password">
-        <Link to="/">Forgot your password?</Link>
-      </p>
+        <Form.Group controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            required
+            isInvalid={!validAcct}
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid" />
+        </Form.Group>
+        <p className="forgot-password">
+          <Link to="/">Forgot your password?</Link>
+        </p>
 
-      <Form.Group controlId="formGroupRememberMe">
-        <Form.Check label="Remember me" />
-      </Form.Group>
+        <Form.Group controlId="formGroupRememberMe">
+          <Form.Check label="Remember me" />
+        </Form.Group>
 
-      <Form.Group>
-        <Button variant="primary" size="sm" type="submit" onClick={checkValidAccount}>
-          LOG IN
-        </Button>
-      </Form.Group>
-    </Form>
+        <Form.Group>
+          <Button variant="primary" size="sm" type="submit" onClick={checkValidAccount}>
+            LOG IN
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
   );
 };
 
