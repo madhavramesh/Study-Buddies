6d328fef8decdeb90c971bd6e6e23eea60af2d61
Akash Singirikonda <akash_singirikonda@brown.edu>
2021-04-10 03:25:27 -0400

    changed to boostrap

---
+++ frontend/src/components/Preferences/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './PartnerPreferencesElements.scss';
+import { ListGroup } from 'react-bootstrap';
+
+const Preferences: React.FC = () => {
+  return (
+    <>
+      <ListGroup>
+        <ListGroup.Item />
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </>
+  );
+};
+
+export default Preferences;
