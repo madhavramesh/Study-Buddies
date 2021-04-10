4c1ee2658e971c79ed3206bfc97cfaa10fc4c38a
Madhav Ramesh <madhavramesh02@gmail.com>
2021-04-10 05:11:02 -0400

    Nav bar styled

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,19 +7,18 @@ import './NavbarElements.scss';
 const NavBar: React.FC = () => {
   return (
     <>
-      <Navbar className="navbar" bg="dark" variant="dark" sticky="top">
-        <Navbar.Brand href="/">
-          <img alt="" src={logo} width="30" height="30" />
-          Chunky Study Buddies
+      <Navbar className="navbar" bg="black" variant="dark" sticky="top">
+        <Navbar.Brand className="navbar-brand" href="/">
+          <img alt="" src={logo} width="60" height="60" />
+          Chunky
         </Navbar.Brand>
-        <Nav className="mr-auto">
+        <Nav className="ml-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link className="separator">|</Nav.Link>
+          <Nav.Link href="/signup">Sign up</Nav.Link>
+          <Nav.Link href="/login">Log in</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
       </Navbar>
     </>
   );
