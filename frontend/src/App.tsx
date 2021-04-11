86b0c65e32f5f46f9fcea8b4a85534293c66702c
Richard Tang <ringtack2002@gmail.com>
2021-04-11 04:27:22 -0400

    Added HomePage to Router; changed animation a little

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import HomePage from './components/HomePage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={NavBar} />
+          <Route exact path="/" component={HomePage} />
           <Route exact path="/footer" component={Footer} />
           <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
