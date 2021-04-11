196b9f9839f3fdb373198d90fb01009b50ef781b
Jack <jackcheng2002@gmail.com>
2021-04-11 10:24:28 -0400

    about page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
           <Route exact path="/" component={HomePage} />
           <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
+          <Route path="/about" component={AboutPage} />
           {/* <Route path="/preferences" component={Preferences} />
           <Route path="/carddemo" component={() => <TimesPane slotLength={30} />} /> */}
         </Switch>
