1782b3530fe180c01bf4130d32a82076c1d205a9
Richard Tang <ringtack2002@gmail.com>
2021-04-11 23:24:25 -0400

    resolved App merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,7 @@ import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
-<<<<<<< HEAD
-import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-=======
 import ClassCard from './components/ClassCard';
 import PeopleCard from './PeopleCard';
 import TimeCard from './components/TimeCard';
@@ -15,7 +12,6 @@ import Preferences from './components/Preferences';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import TestDisplayClassesPage from './pages/TestDisplayClassesPage';
->>>>>>> frontend
 
 const App: React.FC = () => {
   return (
@@ -25,12 +21,9 @@ const App: React.FC = () => {
           <Route exact path="/" component={HomePage} />
           <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
-<<<<<<< HEAD
           <Route path="/about" component={AboutPage} />
-=======
           <Route path="/dashboard" component={DashboardPage} />
           <Route path="/profile" component={Signin} />
->>>>>>> frontend
           {/* <Route path="/preferences" component={Preferences} />
           <Route path="/carddemo" component={() => <TimesPane slotLength={30} />} /> */}
           <Route path="/test_display_classes" component={TestDisplayClassesPage} />
