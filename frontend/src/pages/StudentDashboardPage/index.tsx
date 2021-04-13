79a264e19f91a79b52a7d9366a3819ce148b14a8
Madhav Ramesh <madhavramesh02@gmail.com>
2021-04-12 20:36:51 -0400

    Pre-demo

--- frontend/src/pages/StudentDashboardPage/index.tsx
+++ frontend/src/pages/StudentDashboardPage/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import ModifiedNavBar from '../../components/ModifiedNavbar';
 import ClassCreatedModal from '../../components/ClassCreatedModal';
 import GeneralInfoClass from '../../components/GeneralInfoClass';
 import './StudentDashboard.scss';
 import StudentInfo from '../../components/StudentInfo';
+import PreferencesButton from '../../components/PreferencesButton';
 
 const axios = require('axios');
 
@@ -18,6 +21,10 @@ const StudentDashboardPage: React.FC = ({ match }: any) => {
   const {
     params: { classID },
   } = match;
+
+  const history = useHistory();
+
+  const username = `${sessionStorage.getItem('first_name')} ${sessionStorage.getItem('last_name')}`;
 
   const [className, setClassName] = useState('');
   const [classNumber, setClassNumber] = useState('');
@@ -58,6 +65,29 @@ const StudentDashboardPage: React.FC = ({ match }: any) => {
       });
   };
 
+  const removeStudent = (studentID: string) => {
+    const postParameters = {
+      id: studentID,
+      class_id: classID,
+    };
+
+    axios
+      .post(`http://localhost:4567/leave_class`, postParameters, CONFIG)
+      .then((response: any) => {
+        if (response.data.status === 0) {
+          const studentsCopy = [...students];
+          setStudents(studentsCopy.filter((studentCopy: any) => studentCopy.id !== studentID));
+          history.push('/dashboard');
+          console.log('User successfully removed');
+        } else {
+          console.log('User not allowed to be on this page');
+        }
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     getClassInfo();
     getStudents();
@@ -67,11 +97,23 @@ const StudentDashboardPage: React.FC = ({ match }: any) => {
     });
   }, []);
 
+  const [modalShow, setModalShow] = useState(true);
+
   return (
-    <div>
-      <ModifiedNavBar username="Madhav Ramesh" />
-      <div className="column">
-        <div id="top">
+    <div className="student-dashboard-page">
+      <ModifiedNavBar username={username} />
+
+      <div className="class-joined-modal-container">
+        <ClassCreatedModal
+          onHide={() => setModalShow(false)}
+          show={modalShow}
+          classNumber={classNumber}
+          className={className}
+        />
+      </div>
+
+      <div className="student-dashboard-page-sections">
+        <div className="page-section general-info">
           <GeneralInfoClass
             className={className}
             classNumber={classNumber}
@@ -79,11 +121,32 @@ const StudentDashboardPage: React.FC = ({ match }: any) => {
             classTerm={classTerm}
             classCode={classCode}
           />
+          <div className="page-section select-preferences">
+            <PreferencesButton />
+          </div>
         </div>
-        <div id="bottom" />
-      </div>
 
-      <div className="column" />
+        <div className="page-section students">
+          <div className="students-header">Students</div>
+          <div className="students-body">
+            {students.map((student: any) => (
+              <StudentInfo
+                studentName={`${student.firstName} ${student.lastName}`}
+                removeStudent={() => console.log('No remove student')}
+                removeButton={false}
+              />
+            ))}
+          </div>
+          <div className="leave-class-container">
+            <Button
+              className="leave-class-button"
+              onClick={() => removeStudent(sessionStorage.getItem('user_id')!)}
+            >
+              Leave Class
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
